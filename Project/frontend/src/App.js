98a56ec191b135dddd6edb957cd2656665a7d1b2
Yazan Musahher <mohamed.musahher@gmail.com>
2024-04-11 21:52:57 +0200

    add more routes

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,95 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link, Navigate} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './userModules/HomeModulesAuth/ProtectedRoute';
 
-//AdminModules
+// AdminModules
 import SomeAdminPage from './adminModules/Shared/Components/SomeAdminPage';
-import Header from "./adminModules/Shared/Components/Header/Header";
-import SharedLayout from "./adminModules/Shared/SharedLayout";
-import Sidebar from "./adminModules/Shared/Components/Sidebar/Sidebar";
+import Header from './adminModules/Shared/Components/Header/Header';
+import SharedLayout from './adminModules/Shared/SharedLayout';
+import Sidebar from './adminModules/Shared/Components/Sidebar/Sidebar';
+import Dashboard from './adminModules/Dashboard/Dashboard';
+import Products from './adminModules/Products/Products';
+import ProductPage from './adminModules/Storage/ProductPage';
+import OrderPage from './adminModules/Orders/OrderPage';
+import ProducerProfilePage from './adminModules/ProducerProfilePage';
+import StockComponent from './adminModules/Sales/Components/StockComponent';
+import OrderComponent from './adminModules/Sales/Components/OrderComponent';
+import TransporterComponent from './adminModules/Sales/Components/TransporterComponent';
+import AdminControlComponent from './adminModules/Sales/Components/AdminControlComponent';
 
-import Dashboard from "./adminModules/Dashboard/Dashboard";
-import Products from "./adminModules/Products/Products";
-import ProductPage from "./adminModules/Storage/ProductPage";
-
-import OrderPage from "./adminModules/Orders/OrderPage";
-import ProducerProfilePage from "./adminModules/ProducerProfilePage";
-
-import AdminModule from './adminModules';
-import StockComponent from "./adminModules/Sales/Components/StockComponent";
-import OrderComponent from "./adminModules/Sales/Components/OrderComponent";
-import TransporterComponent from "./adminModules/Sales/Components/TransporterComponent";
-import AdminControlComponent from "./adminModules/Sales/Components/AdminControlComponent";
-
-//AuthModules
+// AuthModules
 import Login from './AuthModules/login';
-import Signup from './AuthModules/signup'
-import TransporterProfilePage from "./adminModules/TransporterProfilePage";
+import Signup from './AuthModules/signup';
+import TransporterProfilePage from './adminModules/TransporterProfilePage';
 
-//HomeModules
+// HomeModules
 import Home from './userModules/HomeModules/Home/Home';
 
-
+// HomeModulesAuth
+import HomeAuth from './userModules/HomeModulesAuth/Home/HomeAuth';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                {/*Routing for HomeModules*/}
-                <Route path="/" element={<Home />} />
-                {/*Routing for AuthModules*/}
-                <Route path="/login/" element={<Login />} />
-                <Route path="/signup/" element={<Signup />} />
-            
-                 {/*Routing for AdminModule*/}
-                <Route path="/admin" element={<SharedLayout />}>
-                    {/* AdminModule is the default child route of "/admin" */}
-                    <Route index element={<Navigate replace to="dashboard" />} />
-                    <Route path="dashboard" element={<Dashboard />} />
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    {/* Home Modules */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
 
+                    {/* Protected Home Module */}
+                    <Route path="/HomeAuth" element={<ProtectedRoute element={HomeAuth} />} />
 
-                    <Route path="stock" element={<StockComponent />} />
-                    <Route path="stock/product-page" element={<ProductPage />} />
-                    <Route path="stock/product-page/:productId" element={<ProductPage />} />
+                    {/* Admin Module Routes */}
+                    <Route path="/admin" element={<SharedLayout />}>
+                        <Route index element={<Navigate replace to="dashboard" />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="stock" element={<StockComponent />} />
+                        <Route path="stock/product-page/:productId" element={<ProductPage />} />
+                        <Route path="products" element={<Products />} />
+                        <Route path="order" element={<OrderComponent />} />
+                        <Route path="order/order-page/:orderId" element={<OrderPage />} />
+                        <Route path="transporter" element={<TransporterComponent />} />
+                        <Route path="transporter/transporter-page/:transporterId" element={<TransporterProfilePage />} />
+                        <Route path="producer-page" element={<ProducerProfilePage />} />
+                        <Route path="adminOverview" element={<AdminControlComponent />} />
+                        <Route path="some-admin-page" element={<SomeAdminPage />} />
+                    </Route>
 
-                    <Route path="products" element={<Products />} />
-
-                    <Route path="order" element={<OrderComponent />} />
-                    <Route path="order/order-page" element={<OrderPage />} />
-                    <Route path="order/order-page/:orderId" element={<OrderPage />} />
-
-
-                    <Route path="transporter" element={<TransporterComponent />} />
-                    <Route path="transporter/transporter-page" element={<TransporterProfilePage />} />
-                    <Route path="transporter/transporter-page/:transporterId" element={<TransporterProfilePage />} />
-
-
-                    <Route path="producer-page" element={<ProducerProfilePage />} />
-
-
-                    <Route path="some-admin-page" element={<SomeAdminPage />} />
-
-                    {/* ... other nested admin routes ... */}
-
-                    <Route path="/admin/stock" element={<StockComponent/>}/>
-                    <Route path="/admin/order" element={<OrderComponent/>}/>
-                    <Route path="/admin/transporter" element={<TransporterComponent/>}/>
-                    <Route path="/admin/adminOverview" element={<AdminControlComponent/>}/>
-
-                </Route>
-                <Route path="/admin" element={<AdminModule />} />
-                <Route path="/admin/some-admin-page" element={<SomeAdminPage />} />
-               
-                
-               
-                <Route path="/admin/test-sharedlayout" element={<SharedLayout />}  />
-                <Route path="/admin/test-header" element={<Header />}  />
-                <Route path="/admin/test-sidebar" element={<Sidebar />}  />
-                <Route path="/admin" element={<AdminModule />} />
-            </Routes>
-        </Router>
+                    {/* Test Routes */}
+                    <Route path="/admin/test-sharedlayout" element={<SharedLayout />} />
+                    <Route path="/admin/test-header" element={<Header />} />
+                    <Route path="/admin/test-sidebar" element={<Sidebar />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
-
 }
 
 export default App;
