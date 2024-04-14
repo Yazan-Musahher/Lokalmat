207468b14b34d918a0729f6a2d903b59d373b9e2
Yazan Musahher <mohamed.musahher@gmail.com>
2024-04-14 21:52:50 +0200

    change the way to store user Role

--- server/Models/AuthModule/ApplicationUser.cs
+++ server/Models/AuthModule/ApplicationUser.cs
@@ -2,12 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace server.Models.AuthModule;
 
-public enum UserType
-{
-    PrivateUser,
-    Manufacturer,
-    LargeHousehold
-}
+
 
 public class ApplicationUser : IdentityUser
 {
@@ -15,5 +10,6 @@ public class ApplicationUser : IdentityUser
     public string? LastName { get; set; }
     public string? Address { get; set; }
     public string? Phone { get; set; }
-    public UserType UserType { get; set; } // User type property
+    public string UserType { get; set; } // Changed from enum UserType to string
 }
+
