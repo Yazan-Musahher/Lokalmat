909ffd0eb92209da0f5f73bdac90c2c04e62385d
Yazan Musahher <mohamed.musahher@gmail.com>
2024-04-14 21:51:36 +0200

    Add sendgrid api to be able to send emails for Password reset

--- server/Controllers/AuthModule/AuthController.cs
+++ server/Controllers/AuthModule/AuthController.cs
@@ -1,14 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using server.Data;
-using server.Models.AuthModule;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using server.Models.AuthModule;
 
 namespace server.Controllers
 {
@@ -32,10 +35,14 @@ namespace server.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(new {message = "Register information is not valid"});
+                return BadRequest(new { message = "Register information is not valid" });
             }
-            
-            // Check if the user already exists
+
+            if (!new HashSet<string> { UserTypes.PrivateUser, UserTypes.Manufacturer, UserTypes.LargeHousehold }.Contains(model.UserType))
+            {
+                return BadRequest(new { message = "Invalid UserType provided." });
+            }
+
             var existingUser = await _userManager.FindByEmailAsync(model.Email);
             if (existingUser != null)
             {
@@ -51,47 +58,99 @@ namespace server.Controllers
                 Address = model.Address,
                 Phone = model.Phone,
                 UserType = model.UserType,
-                EmailConfirmed = true // just temporary, should implement Email confirmation for new users 
+                EmailConfirmed = true
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
+            if (!result.Succeeded)
             {
-                // You might want to generate an email confirmation token here
-                // and send it to the user before marking them as confirmed.
-                return Ok(new { message = "User created successfully" });
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return BadRequest(ModelState);
             }
 
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            } 
-            return BadRequest(ModelState);
+            return Ok(new { message = "User created successfully" });
         }
-
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserLoginModel model)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(new {message = "Login information is not valid"});
+                return BadRequest(new { message = "Login information is not valid" });
             }
-            
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, lockoutOnFailure: false);
 
-            if (result.Succeeded)
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            if (!result.Succeeded)
             {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-                var token = GenerateJwtToken(user);
-
-                return Ok(new { token = token });
+                ModelState.AddModelError(string.Empty, "Incorrect username or password.");
+                return BadRequest(ModelState);
             }
-            
-            ModelState.AddModelError(string.Empty, "Incorrect username or password.");
-            return BadRequest(ModelState);
-            
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            var token = GenerateJwtToken(user);
+            return Ok(new { token = token, email = user.Email, name = user.Name, role = user.UserType });
+        }
+        // start of managing request password for user
+        [HttpPost("requestPasswordReset")]
+        public async Task<IActionResult> RequestPasswordReset(PasswordReset model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Invalid email address" });
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Bruker finnes ikke" });
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = $"http://localhost:3000/Password-Reset?token={Uri.EscapeDataString(token)}&email={Uri.EscapeDataString(user.Email)}";
+            await SendPasswordResetEmail(model.Email, callbackUrl);
+
+            return Ok(new { message = "Password reset link has been sent to your email address." });
+        }
+        
+        [HttpPost("resetPassword")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Invalid request" });
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "User not found" });
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.NewPassword);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(e => e.Description));
+            }
+
+            return Ok(new { message = "Password has been successfully reset." });
+        }
+
+
+        private async Task SendPasswordResetEmail(string email, string link)
+        {
+            var apiKey = _configuration["SendGrid:ApiKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("contact@yazan-musahher.no", "Lokalmat");
+            var subject = "Reset Your Password";
+            var to = new EmailAddress(email);
+            var plainTextContent = $"Please reset your password by clicking here: {link}";
+            var htmlContent = $"<strong>Please reset your password by clicking <a href='{link}'>here</a></strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            // Optionally handle the response
         }
 
         private string GenerateJwtToken(ApplicationUser user)
@@ -189,6 +248,7 @@ namespace server.Controllers
             
         }
         
+        
         [HttpPut("updatePassword")]
         public async Task<IActionResult> UpdatePassword(UpdatePasswordModel model)
         {
@@ -216,7 +276,11 @@ namespace server.Controllers
             return Ok(new { token = token });
             
         }
+        
+        
+        
     }
+    
 
     public class UserSignupModel
     {
@@ -226,8 +290,15 @@ namespace server.Controllers
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
-        public UserType UserType { get; set; }
+        public string UserType { get; set; }
     }
+    public static class UserTypes
+    {
+        public const string PrivateUser = "PrivateUser";
+        public const string Manufacturer = "Manufacturer";
+        public const string LargeHousehold = "LargeHousehold";
+    }
+
 
     public class UserLoginModel
     {
